e3e5815bcecb59d62215052e693f85bfb0b7be02
Fnr <ah2014bh@outlook.com>
2022-02-27 00:32:34 +0300

    feat: convert video views, dates

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { default: axios } = require('axios');
+const { millify } = require('millify');
+const humanizeDuration = require('humanize-duration');
 
 router.get('/', async(req, res) => {
     const data = (await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=SA&key=${process.env.YOUTUBE_KEY}&maxResults=8`)).data;
     res.render('pages/index', {
-        data: data
+        data: data,
+        converter: millify,
+        humanizeDuration: humanizeDuration
     });
 })
 
